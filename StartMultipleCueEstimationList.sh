618bf4f5fb467ebb164bbe131af6742c2de2af51
Johannes <jjojo.k@googlemail.com>
2015-10-26 21:52:41 +0100

    eval

--- StartMultipleCueEstimationList.sh
+++ StartMultipleCueEstimationList.sh
@@ -16,44 +16,43 @@ echo "first image, second image, featureCountCombined, trueFeatureCountCombined,
 #sh MultipleCueEstimationList.sh "castle-P30/0000.png" "castle-P30/0003.png" $OUT
 #sh MultipleCueEstimationList.sh "castle-P30/0002.png" "castle-P30/0003.png" $OUT
 #sh MultipleCueEstimationList.sh "castle-P30/0001.png" "castle-P30/0002.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0003.png" "castle-P30/0004.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0004.png" "castle-P30/0005.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0005.png" "castle-P30/0006.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0008.png" "castle-P30/0009.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0013.png" "castle-P30/0014.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0014.png" "castle-P30/0015.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0015.png" "castle-P30/0016.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0020.png" "castle-P30/0021.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0021.png" "castle-P30/0022.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0018.png" "castle-P30/0019.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0026.png" "castle-P30/0028.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0025.png" "castle-P30/0026.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0026.png" "castle-P30/0027.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0004.png" "castle-P30/0006.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0016.png" "castle-P30/0018.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0003.png" "castle-P30/0004.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0004.png" "castle-P30/0005.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0005.png" "castle-P30/0006.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0008.png" "castle-P30/0009.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0013.png" "castle-P30/0014.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0014.png" "castle-P30/0015.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0015.png" "castle-P30/0016.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0020.png" "castle-P30/0021.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0021.png" "castle-P30/0022.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0018.png" "castle-P30/0019.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0026.png" "castle-P30/0028.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0025.png" "castle-P30/0026.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0026.png" "castle-P30/0027.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0004.png" "castle-P30/0006.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0016.png" "castle-P30/0018.png" $OUT
 #"should not work" examples
-sh MultipleCueEstimationList.sh "castle-P30/0005.png" "castle-P30/0007.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0023.png" "castle-P30/0024.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0028.png" "castle-P30/0029.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0001.png" "castle-P30/0007.png" $OUT
-sh MultipleCueEstimationList.sh "castle-P30/0003.png" "castle-P30/0006.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0005.png" "castle-P30/0007.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0023.png" "castle-P30/0024.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0028.png" "castle-P30/0029.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0001.png" "castle-P30/0007.png" $OUT
+#sh MultipleCueEstimationList.sh "castle-P30/0003.png" "castle-P30/0006.png" $OUT
 
 ###################### Entry ####################################################
-sh MultipleCueEstimationList.sh "" ""  $OUT
-sh MultipleCueEstimationList.sh "entry-P10/0000.png" "entry-P10/0001.png" $OUT
-sh MultipleCueEstimationList.sh "entry-P10/0008.png" "entry-P10/0009.png" $OUT
+#sh MultipleCueEstimationList.sh "entry-P10/0000.png" "entry-P10/0001.png" $OUT
+#sh MultipleCueEstimationList.sh "entry-P10/0008.png" "entry-P10/0009.png" $OUT
 #"should not work" examples
-sh MultipleCueEstimationList.sh "entry-P10/0007.png" "entry-P10/0009.png" $OUT
-sh MultipleCueEstimationList.sh "entry-P10/0007.png" "entry-P10/0008.png" $OUT
-sh MultipleCueEstimationList.sh "entry-P10/0003.png" "entry-P10/0005.png" $OUT
-sh MultipleCueEstimationList.sh "entry-P10/0005.png" "entry-P10/0006.png" $OUT
+#sh MultipleCueEstimationList.sh "entry-P10/0007.png" "entry-P10/0009.png" $OUT
+#sh MultipleCueEstimationList.sh "entry-P10/0007.png" "entry-P10/0008.png" $OUT
+#sh MultipleCueEstimationList.sh "entry-P10/0003.png" "entry-P10/0005.png" $OUT
+#sh MultipleCueEstimationList.sh "entry-P10/0005.png" "entry-P10/0006.png" $OUT
 
 ###################### Fountain ####################################################
 
-sh MultipleCueEstimationList.sh "fountain-P11/0000.png" "fountain-P11/0001.png" $OUT
-sh MultipleCueEstimationList.sh "fountain-P11/0002.png" "fountain-P11/0003.png" $OUT
-sh MultipleCueEstimationList.sh "fountain-P11/0004.png" "fountain-P11/0005.png" $OUT
-sh MultipleCueEstimationList.sh "fountain-P11/0005.png" "fountain-P11/0006.png" $OUT
+#sh MultipleCueEstimationList.sh "fountain-P11/0000.png" "fountain-P11/0001.png" $OUT
+#sh MultipleCueEstimationList.sh "fountain-P11/0002.png" "fountain-P11/0003.png" $OUT
+#sh MultipleCueEstimationList.sh "fountain-P11/0004.png" "fountain-P11/0005.png" $OUT
+#sh MultipleCueEstimationList.sh "fountain-P11/0005.png" "fountain-P11/0006.png" $OUT
 sh MultipleCueEstimationList.sh "fountain-P11/0000.png" "fountain-P11/0003.png" $OUT
 sh MultipleCueEstimationList.sh "fountain-P11/0008.png" "fountain-P11/0009.png" $OUT
 sh MultipleCueEstimationList.sh "fountain-P11/0009.png" "fountain-P11/0010.png" $OUT
---
+++ evaluation.m
@@ -0,0 +1,167 @@
+%featureCountCombined, 
+%trueFeatureCountCombined
+%Fgt: trueSampsonErr
+%Fgt: trueErrorStdDev
+%Fgt: trueRootSampsonErr, 
+%Fgt: trueRootErrorStdDev,
+%refined F: inlierCountCombined, 
+%refined F: trueInlierCountCombined, 
+%refined F: sampsonErrCombined, 
+%refined F: trueSampsonErr, 
+%refined F: trueSampsonErrStdDev, 
+%refined F: trueRootSampsonErr, 
+%refined F: trueRootSampsonErrStdDev, 
+%F_Points: featureCountGood, 
+%F_Points: trueFeatureCountGood, 
+%F_Points: featureCountComplete, 
+%F_Points: trueFeatureCountComplete, 
+%F_Points: inlierCountOwnGood, 
+%F_Points: trueInlierCountOwnGood, 
+%F_Points: inlierCountOwnComplete, 
+%F_Points: trueInlierCountOwnComplete, 
+%F_Points: inlierCountCombined, 
+%F_Points: trueInlierCountCombined, 
+%F_Points: sampsonErrOwn, 
+%F_Points: sampsonErrComplete, 
+%F_Points: sampsonErrCombined, 
+%F_Points: sampsonErrStdDevCombined, 
+%F_Points: trueSampsonErr, 
+%F_Points: trueSampsonErrStdDev, 
+%F_Points: trueRootSampsonErr, 
+%F_Points: trueRootSampsonErrStdDev, 
+%F_Points: quality, 
+%H_Lines: featureCountGood, 
+%H_Lines: trueFeatureCountGood, 
+%H_Lines: featureCountComplete, 
+%H_Lines: trueFeatureCountComplete, 
+%H_Lines: inlierCountOwnGood, 
+%H_Lines: trueInlierCountOwnGood, 
+%H_Lines: inlierCountOwnComplete, 
+%H_Lines: trueInlierCountOwnComplete, 
+%H_Lines: inlierCountCombined, 
+%H_Lines: trueInlierCountCombined, 
+%H_Lines: sampsonErrOwn, 
+%H_Lines: sampsonErrComplete, 
+%H_Lines: sampsonErrCombined, 
+%H_Lines: sampsonErrStdDevCombined, 
+%H_Lines: trueSampsonErr, 
+%H_Lines: trueSampsonErrStdDev, 
+%H_Lines: trueRootSampsonErr, 
+%H_Lines: trueRootSampsonErrStdDev, 
+%H_Lines: quality, 
+%H_Points: featureCountGood, 
+%H_Points: trueFeatureCountGood, 
+%H_Points: featureCountComplete, 
+%H_Points: trueFeatureCountComplete, 
+%H_Points: inlierCountOwnGood, 
+%H_Points: trueInlierCountOwnGood, 
+%H_Points: inlierCountOwnComplete, 
+%H_Points: trueInlierCountOwnComplete, 
+%H_Points: inlierCountCombined, 
+%H_Points: trueInlierCountCombined, 
+%H_Points: sampsonErrOwn, 
+%H_Points: sampsonErrComplete, 
+%H_Points: sampsonErrCombined,  
+%H_Points: sampsonErrStdDevCombined, 
+%H_Points: trueSampsonErr, 
+%H_Points: trueSampsonErrStdDev, 
+%H_Points: trueRootSampsonErr, 
+%H_Points: trueRootSampsonErrStdDev, 
+%H_Points: quality, 
+%time (min)
+
+%first image, second image, featureCountCombined,, trueFeatureCountCombined,, Fgt: trueSampsonErr,, Fgt: trueErrorStdDev,, Fgt: trueRootSampsonErr,, Fgt: trueRootErrorStdDev,, refined F: inlierCountCombined,, refined F: trueInlierCountCombined,, refined F: sampsonErrCombined,, refined F: trueSampsonErr,, refined F: trueSampsonErrStdDev,, refined F: trueRootSampsonErr,, refined F: trueRootSampsonErrStdDev,, F_Points: featureCountGood,, F_Points: trueFeatureCountGood,, F_Points: featureCountComplete,, F_Points: trueFeatureCountComplete,, F_Points: inlierCountOwnGood,, F_Points: trueInlierCountOwnGood,, F_Points: inlierCountOwnComplete,, F_Points: trueInlierCountOwnComplete,, F_Points: inlierCountCombined,, F_Points: trueInlierCountCombined,, F_Points: sampsonErrOwn,, F_Points: sampsonErrComplete,, F_Points: sampsonErrCombined,, F_Points: sampsonErrStdDevCombined,, F_Points: trueSampsonErr,, F_Points: trueSampsonErrStdDev,, F_Points: trueRootSampsonErr,, F_Points: trueRootSampsonErrStdDev,, F_Points: quality,, H_Lines: featureCountGood,, H_Lines: trueFeatureCountGood,, H_Lines: featureCountComplete,, H_Lines: trueFeatureCountComplete,, H_Lines: inlierCountOwnGood,, H_Lines: trueInlierCountOwnGood,, H_Lines: inlierCountOwnComplete,, H_Lines: trueInlierCountOwnComplete,, H_Lines: inlierCountCombined,, H_Lines: trueInlierCountCombined,, H_Lines: sampsonErrOwn,, H_Lines: sampsonErrComplete,, H_Lines: sampsonErrCombined,, H_Lines: sampsonErrStdDevCombined,, H_Lines: trueSampsonErr,, H_Lines: trueSampsonErrStdDev,, H_Lines: trueRootSampsonErr,, H_Lines: trueRootSampsonErrStdDev,, H_Lines: quality,, H_Points: featureCountGood,, H_Points: trueFeatureCountGood,, H_Points: featureCountComplete,, H_Points: trueFeatureCountComplete,, H_Points: inlierCountOwnGood,, H_Points: trueInlierCountOwnGood,, H_Points: inlierCountOwnComplete,, H_Points: trueInlierCountOwnComplete,, H_Points: inlierCountCombined,, H_Points: trueInlierCountCombined,, H_Points: sampsonErrOwn,, H_Points: sampsonErrComplete,, H_Points: sampsonErrCombined,, H_Points: sampsonErrStdDevCombined,, H_Points: trueSampsonErr,, H_Points: trueSampsonErrStdDev,, H_Points: trueRootSampsonErr,, H_Points: trueRootSampsonErrStdDev,, H_Points: quality,, time (min)
+
+%function [rms, stddev] = rootMeanStdDev(x)
+%	res = 0;
+%	for i = 1:rows(data)
+		
+%	endfor
+%endfunction
+
+data = csvread("Results/resultList_test.csv");
+result = zeros(rows(data)/10, 2*columns(data));
+result_small = zeros(rows(data)/10, 10);
+evaluation_small = zeros(100, 10);
+k = 1;
+for i = 1:10:rows(data) 
+	for j = 1:columns(data)
+		col = data([i:i+9],j);
+		#printf("rows: %f\n", i);
+	    #printf("z(%d,%d) = %f\n", i, j, z(i,j));
+		
+		if (j == 3 || j == 9 || j == 10 || j == 24 || j == 25 || j == 26 || j == 28 || j == 43 || j == 44 || j == 45 || j == 47 || j == 62 || j  == 63 || j == 64 || j == 66)
+			sqrt(col);
+		endif
+
+		if(!isnumeric(col(1,1)) || col(1,1) == -1) 
+			result(k, 2*j-1) = -1;
+			result(k, 2*j) = -1;
+		else
+			result(k, 2*j-1) = mean(col);
+			result(k, 2*j) = std(col);
+		endif
+
+		if (j == 3)
+			result_small(k,1) = result(k, 2*j-1);
+			result_small(k,2) = result(k, 2*j-1);
+		elseif (j == 10)
+			result_small(k,3) = result(k, 2*j-1);
+			result_small(k,4) = result(k, 2*j-1);
+		elseif (j == 28)
+			result_small(k,5) = result(k, 2*j-1);
+			result_small(k,6) = result(k, 2*j-1);
+		elseif (j == 47)
+			result_small(k,7) = result(k, 2*j-1);
+			result_small(k,8) = result(k, 2*j-1);
+		elseif (j == 66)
+			result_small(k,9) = result(k, 2*j-1);
+			result_small(k,10) = result(k, 2*j-1);
+		endif
+	
+	endfor
+	k++;	
+endfor
+
+k=1;
+for t = 0.1:0.1:10
+	evaluation_small(k, 2) = t;
+	for j = 3:2:columns(result_small)
+		for i = 1:rows(result_small) 
+			if(result_small(i,j) > 0.01 && result_small(i,j) < t)
+				evaluation_small(k, j) += result_small(i,j);
+				evaluation_small(k, j + 1)++;
+			endif	
+		endfor 
+		if(evaluation_small(k, j + 1) > 0) 
+			evaluation_small(k, j) / evaluation_small(k, j + 1);
+		endif
+	endfor
+	k++;
+endfor 
+
+%Fgt: trueSampsonErr,,
+msize=6;
+lwidth=1.5;
+
+h1=figure;#("Position",[10,10,1200,800]);
+plot (evaluation_small([1:100],2), evaluation_small([1:100],3), "-ko", "markersize", msize, "linewidth", lwidth, evaluation_small([1:100],2), evaluation_small([1:100],5), "-go", "markersize", msize, "linewidth", lwidth,  evaluation_small([1:100],2), evaluation_small([1:100],7), "-bo", "markersize", msize, "linewidth", lwidth, evaluation_small([1:100],2), evaluation_small([1:100],9), "-ro", "markersize", msize, "linewidth", lwidth);
+
+#title ("");
+xlabel ("Error threshold");
+ylabel ("Example mean RMS error");
+legend ("final result", "from points", "from line homog.", "from point homog.", "location", "northwest");
+
+h2=figure;#("Position",[10,10,1200,800]);
+plot (evaluation_small([1:100],2), evaluation_small([1:100],4), "--ko", "markersize", msize, "linewidth", lwidth, evaluation_small([1:100],2), evaluation_small([1:100],6), "--go", "markersize", msize, "linewidth", lwidth, evaluation_small([1:100],2), evaluation_small([1:100],8), "--bo", "markersize", msize, "linewidth", lwidth, evaluation_small([1:100],2), evaluation_small([1:100],10), "--ro", "markersize", msize, "linewidth", lwidth, "markersize", msize, "linewidth", lwidth); 
+
+xlabel ("Error threshold");
+ylabel ("Number of examples with mean RMS error below threshold");
+legend ("final result", "from points", "from line homog.", "from point homog.", "location", "east");
+
+#saveas (h1, "evaluation_small1.png");
+#saveas (h2, "evaluation_small2.png");
+
+csvwrite("es_inlier_error_graph.csv", result);
+csvwrite("evaluation_small.csv", result);
+csvwrite("evaluation.csv", result);
